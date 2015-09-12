304bedd78c86a368d86707604e0f4cbc2a894f53
pieszynski <git@pieszynski.com>
2015-09-12 21:27:22 +0200

    AppCache

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # RadarOpadowy
 Aplikacja internetowa do oglądania ostatnich kilku map radarowych Polski
-
-### W końcowej wersji odkomentować użycie manifestu i AppCache!
--- js/index.js
+++ js/index.js
@@ -10,5 +10,17 @@
 	appCache.oncached = function onCachedEvt(evt) {
 		console.log('app is cached!', appCache.status);
 	}
+	appCache.onupdateready = function onUpdateReadyEvt(evt) {
+		console.log('update ready!', appCache.status, appCache.UPDATEREADY);
+
+		//appCache.update(); // ręczne wymuszenie aktualizacji
+		
+		if (appCache.UPDATEREADY === appCache.status) {	// === 4
+			appCache.swapCache();
+			window.location.reload();
+		}
+	}
+
+	console.log(appCache.status);
 
 })(window);
